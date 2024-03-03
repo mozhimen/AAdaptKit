b5d7bdafb4331b3e45d9f67a9030bb5b9bb59a09
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-03-04 01:03:37 +0800

    0304

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # AAdaptKit
 Android适配库 -> 状态栏, 机型适配
+
+```groovy
+//AAdaptKit
+include ":AAdaptKit"
+project(':AAdaptKit').projectDir = new File("D:\\WorkSpace\\GitHub\\AAdaptKit")
+include ':AAdaptKit:adaptk_dynamic_colors'
+```
---
+++ adaptk_dynamic_colors/src/main/java/com/mozhimen/adaptk/dynamic/colors/AdaptKDynamicColors.kt
@@ -0,0 +1,23 @@
+package com.mozhimen.adaptk.dynamic.colors
+
+import android.annotation.TargetApi
+import android.app.Application
+import com.google.android.material.color.DynamicColors
+import com.mozhimen.basick.elemk.android.os.cons.CVersCode
+import com.mozhimen.uicorek.optins.OTheme_ThemeMaterial3DayNoAction
+
+/**
+ * @ClassName AdaptKDynamicColors
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2024/3/3 23:11
+ * @Version 1.0
+ */
+object AdaptKDynamicColors {
+    @JvmStatic
+    @TargetApi(CVersCode.V_31_12_S)
+    @OTheme_ThemeMaterial3DayNoAction
+    fun init(application: Application) {
+        DynamicColors.applyToActivitiesIfAvailable(application)
+    }
+}
