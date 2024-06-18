e6fd2d03f885b608708524e62d824a9b6a0cae2e
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-06-18 11:04:59 +0800

    0618

---
+++ adaptk_splashscreen/src/main/java/com/mozhimen/adaptk/splashscreen/common/BaseSplashScreenActivityVBVM.kt
@@ -0,0 +1,15 @@
+package com.mozhimen.adaptk.splashscreen.common
+
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+import com.mozhimen.basick.elemk.androidx.appcompat.bases.viewbinding.BaseActivityVBVM
+
+/**
+ * @ClassName BaseSplashScreenActivityVBVM
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2024/6/17 23:55
+ * @Version 1.0
+ */
+abstract class BaseSplashScreenActivityVBVM<VB : ViewBinding, VM : ViewModel> : BaseActivityVBVM<VB, VM>() {
+}
