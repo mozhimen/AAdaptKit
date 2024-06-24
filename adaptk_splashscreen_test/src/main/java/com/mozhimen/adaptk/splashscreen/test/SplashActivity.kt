1fbd43ac6d793103424398eef39ba405a6b331c8
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-06-24 09:45:47 +0800

    0624

---
+++ adaptk_splashscreen_test/src/main/java/com/mozhimen/adaptk/splashscreen/test/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.mozhimen.adaptk.splashscreen.test
+
+/**
+ * @ClassName SplashActivity
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2024/6/23 16:34
+ * @Version 1.0
+ */
+class SplashActivity {
+}
