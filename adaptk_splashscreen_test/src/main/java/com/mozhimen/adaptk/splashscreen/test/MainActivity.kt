f1a577602ab590d2bd86fae97e9f77a9cc186835
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-06-25 19:54:14 +0800

    0625

--- adaptk_splashscreen_test/src/main/java/com/mozhimen/adaptk/splashscreen/test/MainActivity.kt
+++ adaptk_splashscreen_test/src/main/java/com/mozhimen/adaptk/splashscreen/test/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.mozhimen.adaptk.splashscreen.test
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.mozhimen.basick.utilk.wrapper.UtilKAnim
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        UtilKAnim.applyActivityAnim(this, android.R.transition.slide_top)
+//        UtilKAnim.applyActivityTransition(this, android.R.transition.slide_top,null)
 //        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
--- adaptk_splashscreen_test/src/main/java/com/mozhimen/adaptk/splashscreen/test/SplashActivity2.kt
+++ adaptk_splashscreen_test/src/main/java/com/mozhimen/adaptk/splashscreen/test/SplashActivity2.kt
@@ -1,32 +1,20 @@
 package com.mozhimen.adaptk.splashscreen.test
 
-import android.animation.Animator
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.graphics.Path
 import android.os.Bundle
-import android.os.SystemClock
-import android.view.View
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.AnticipateInterpolator
+import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.splashscreen.SplashScreenViewProvider
-import androidx.core.view.postDelayed
 import androidx.lifecycle.lifecycleScope
-import com.mozhimen.adaptk.splashscreen.test.databinding.ActivitySplashBinding
 import com.mozhimen.adaptk.splashscreen.utils.AdaptKSplashscreenUtil
-import com.mozhimen.basick.elemk.androidx.appcompat.bases.viewbinding.BaseActivityVBVM
 import com.mozhimen.basick.utilk.android.content.startActivityAndFinish
-import com.mozhimen.basick.utilk.android.content.startActivityAndFinishAnimation_ofActivityOptions
-import com.mozhimen.basick.utilk.android.os.UtilKBuildVersion
+import com.mozhimen.basick.utilk.android.transition.UtilKSlide
 import com.mozhimen.basick.utilk.android.util.UtilKLogWrapper
+import com.mozhimen.basick.utilk.androidx.core.UtilKActivityOptionsCompat
 import com.mozhimen.basick.utilk.commons.IUtilK
+import com.mozhimen.basick.utilk.wrapper.UtilKAnim
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import com.mozhimen.animk.R
 
 /**
  * @ClassName SplashActivity
@@ -36,15 +24,16 @@ import com.mozhimen.animk.R
  * @Version 1.0
  */
 class SplashActivity2 : AppCompatActivity(), IUtilK {
-    var _somethingDelay: Boolean = false
+    var _somethingDelay: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        UtilKAnim.applyActivityTransition(this, null, UtilKSlide.get(Gravity.BOTTOM, 1000), true)
         val splashScreen = installSplashScreen()
         AdaptKSplashscreenUtil.keepSplashScreenLonger(splashScreen) {
             lifecycleScope.launch {
                 delay(6000)
-                _somethingDelay =  true
+                _somethingDelay = false
             }
             return@keepSplashScreenLonger _somethingDelay
         }
@@ -56,8 +45,15 @@ class SplashActivity2 : AppCompatActivity(), IUtilK {
         splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
             UtilKLogWrapper.d(TAG, "SplashScreen#onSplashScreenExit view:$splashScreenViewProvider" + " view:${splashScreenViewProvider.view}" + " icon:${splashScreenViewProvider.iconView}")
 
-            startActivityAndFinish<MainActivity>()
-            overridePendingTransition(R.anim.animk_trans_top_show,R.anim.animk_set_trans_bottom_alpha_hide)
+            startActivityAndFinish<MainActivity>(
+                UtilKActivityOptionsCompat.makeCustomAnimation(
+                    this,
+                    com.mozhimen.animk.R.anim.animk_alpha_show,
+                    com.mozhimen.animk.R.anim.animk_set_trans_bottom_alpha_hide
+                ).toBundle()
+            )
+//            startActivityAndFinishAnimation_ofActivityOptions<MainActivity>()
+//            overridePendingTransition(R.anim.animk_trans_top_show, R.anim.animk_set_trans_bottom_alpha_hide)
         }
     }
 }
