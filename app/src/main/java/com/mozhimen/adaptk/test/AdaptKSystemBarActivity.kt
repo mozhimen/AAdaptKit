07cf7203421a2ced8d5069b75240f1abe184b0b9
Kolin Zhao & Mozhimen <mozhimen@163.com>
2024-06-29 23:12:54 +0800

    0629

--- app/src/main/java/com/mozhimen/adaptk/test/AdaptKSystemBarActivity.kt
+++ app/src/main/java/com/mozhimen/adaptk/test/AdaptKSystemBarActivity.kt
@@ -9,10 +9,10 @@ import com.mozhimen.adaptk.systembar.initAdaptKSystemBar
 import com.mozhimen.adaptk.test.databinding.ActivityAdaptkSystembarBinding
 
 //简单用法, 直接使用预制的属性
-//@ASenseKSystemBarProperty(CProperty.IMMERSED_HARD_STICKY)
+@AAdaptKSystemBarProperty(CProperty.IMMERSED_HARD_STICKY)
 //高级用法自己组合
-@AAdaptKSystemBarProperty(CProperty.NORMAL)
-@AAdaptKSystemBarPropertyOr(CPropertyOr.IMMERSED_OPEN, CPropertyOr.HIDE_STATUS_BAR, CPropertyOr.HIDE_NAVIGATION_BAR)
+//@AAdaptKSystemBarProperty(CProperty.NORMAL)
+//@AAdaptKSystemBarPropertyOr(CPropertyOr.IMMERSED_OPEN, CPropertyOr.HIDE_STATUS_BAR, CPropertyOr.HIDE_NAVIGATION_BAR)
 class AdaptKSystemBarActivity : BaseActivityVDB<ActivityAdaptkSystembarBinding>() {
     override fun initFlag() {
         initAdaptKSystemBar()
