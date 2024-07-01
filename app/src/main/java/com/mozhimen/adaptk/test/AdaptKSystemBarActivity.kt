f47a5b8a307f56cdb4a14876343c6f3c4c2cab6a
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-07-01 09:17:11 +0800

    0701

--- app/src/main/java/com/mozhimen/adaptk/test/AdaptKSystemBarActivity.kt
+++ app/src/main/java/com/mozhimen/adaptk/test/AdaptKSystemBarActivity.kt
@@ -3,25 +3,50 @@ package com.mozhimen.adaptk.test
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.Constraint
 import com.mozhimen.basick.elemk.androidx.appcompat.bases.databinding.BaseActivityVDB
 import com.mozhimen.adaptk.systembar.annors.AAdaptKSystemBarProperty
+import com.mozhimen.adaptk.systembar.annors.AAdaptKSystemBarPropertyAnd
 import com.mozhimen.adaptk.systembar.annors.AAdaptKSystemBarPropertyOr
 import com.mozhimen.adaptk.systembar.cons.CProperty
+import com.mozhimen.adaptk.systembar.cons.CPropertyAnd
 import com.mozhimen.adaptk.systembar.cons.CPropertyOr
 import com.mozhimen.adaptk.systembar.initAdaptKSystemBar
 import com.mozhimen.adaptk.test.databinding.ActivityAdaptkSystembarBinding
+import com.mozhimen.basick.utilk.android.app.getContentView
+import com.mozhimen.basick.utilk.android.app.getDecorView
 
 //简单用法, 直接使用预制的属性
-@AAdaptKSystemBarProperty(CProperty.IMMERSED_HARD_STICKY)
+//@AAdaptKSystemBarProperty(CProperty.IMMERSED_HARD_STICKY)
 //高级用法自己组合
-//@AAdaptKSystemBarProperty(CProperty.NORMAL)
-//@AAdaptKSystemBarPropertyOr(CPropertyOr.IMMERSED_OPEN, CPropertyOr.HIDE_STATUS_BAR, CPropertyOr.HIDE_NAVIGATION_BAR)
+@AAdaptKSystemBarProperty(CProperty.IMMERSED_HARD_STICKY)
+@AAdaptKSystemBarPropertyAnd(CPropertyAnd.NO_OVERLAY_NAVIGATION_BAR,CPropertyAnd.NO_OVERLAY_STATUS_BAR)
 class AdaptKSystemBarActivity : BaseActivityVDB<ActivityAdaptkSystembarBinding>() {
     override fun initFlag() {
         initAdaptKSystemBar()
     }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        printViewInfo(hasFocus)
+    }
+
+    private fun printViewInfo(hasFocus: Boolean?){
+        Log.d(TAG, "onWindowFocusChanged: hasFocus $hasFocus")
+        Log.d(TAG, "onWindowFocusChanged: decorview height ${getDecorView<View>().height}")
+        Log.d(TAG, "onWindowFocusChanged: contentView height ${getContentView<View>().height}")
+        Log.d(TAG, "onWindowFocusChanged: rootView height ${findViewById<ConstraintLayout>(R.id.root)?.height}")
+        Log.d(TAG, "onWindowFocusChanged: view1 y ${vdb.adaptkTxtLeftBottom.y}")
+        Log.d(TAG, "onWindowFocusChanged: view2 y ${vdb.view.height}")
+    }
+
     override fun initView(savedInstanceState: Bundle?) {
+        vdb.adaptkTxt.post {
+            printViewInfo(null)
+        }
         vdb.adaptkTxt.setOnClickListener {
             AlertDialog.Builder(this).show()
         }
